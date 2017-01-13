105b869e0a16fd8245921d90014037f874562d4e
Chris Clifford <cclifford82@gmail.com>
2017-01-13 09:21:26 -0800

    add routing to create new answer

--- app/components/new-answer.js
+++ app/components/new-answer.js
@@ -1,4 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addAnswerTag: false,
+
+  actions: {
+    showAnswerForm() {
+      this.set('addAnswerTag', true);
+    },
+
+    saveAnswer() {
+      var params = {
+        author: this.get('author'),
+        content: this.get('content'),
+        notes: this.get('notes'),
+        date: Date.now(),
+        question: this.get('question')
+      };
+
+      this.set('addAnswerTag', false);
+      this.sendAction('saveAnswer', params);
+    }
+  }
 });
