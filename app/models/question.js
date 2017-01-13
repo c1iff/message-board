fbca08b6fecf9a44fa7a278e5eb3ed12a47c3623
Chris Clifford <cclifford82@gmail.com>
2017-01-12 18:07:37 -0800

    add initial files

---
+++ app/models/question.js
@@ -0,0 +1,10 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  author: DS.attr(),
+  asked: DS.attr(),
+  content: DS.attr(),
+  notes: DS.attr(),
+  votes: DS.attr(),
+  answers: DS.hasMany('answer', { async: true })
+});
---
+++ config/environment.js
@@ -0,0 +1,54 @@
+/* jshint node: true */
+
+module.exports = function(environment) {
+  var ENV = {
+    modulePrefix: 'message-board',
+    environment: environment,
+    rootURL: '/',
+    locationType: 'auto',
+    EmberENV: {
+      FEATURES: {
+        // Here you can enable experimental features on an ember canary build
+        // e.g. 'with-controller': true
+      }
+    },
+
+    firebase: {
+      apiKey: "AIzaSyDjM5YzvLKl6DRtMMHUdRqw9hTIf_LD8OM",
+      authDomain: "message-board-f2a70.firebaseapp.com",
+      databaseURL: "https://message-board-f2a70.firebaseio.com",
+      storageBucket: "message-board-f2a70.appspot.com",
+      messagingSenderId: "620014688891"
+  },
+
+    APP: {
+      // Here you can pass flags/options to your application instance
+      // when it is created
+    }
+  };
+
+  if (environment === 'development') {
+    // ENV.APP.LOG_RESOLVER = true;
+    // ENV.APP.LOG_ACTIVE_GENERATION = true;
+    // ENV.APP.LOG_TRANSITIONS = true;
+    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+    // ENV.APP.LOG_VIEW_LOOKUPS = true;
+  }
+
+  if (environment === 'test') {
+    // Testem prefers this...
+    ENV.locationType = 'none';
+
+    // keep test console output quieter
+    ENV.APP.LOG_ACTIVE_GENERATION = false;
+    ENV.APP.LOG_VIEW_LOOKUPS = false;
+
+    ENV.APP.rootElement = '#ember-testing';
+  }
+
+  if (environment === 'production') {
+
+  }
+
+  return ENV;
+};
